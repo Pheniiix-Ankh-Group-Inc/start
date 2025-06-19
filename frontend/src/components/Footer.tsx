79d401d0e683d689aee1673603a693c8d7f88ce6
WhereRUBeen <brice.mimifir.a@gmail.com>
2025-06-18 22:30:46 -0400

    Feature setup project start (#2)
    * Setup Projet
    
    * Add Conponents site
    
    * Add changelog
    
    * Update projet
    
    * Update projet
    
    ---------
    
    Co-authored-by: WhereRUBeen <whererubeen@MacBook-Air-de-WhereRUBeen.local>
    Co-authored-by: WhereRUBeen <whererubeen@macbookair.home>

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="flex justify-center p-6">
+      <p>©Copyright 2025. All rights reversed.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="flex justify-center ">
+      <img src="" alt="Logo" className="" />
+      <span className="">BRICE MIMIFIR</span>
+    </header>
+  );
+}
+
+export default Header;
